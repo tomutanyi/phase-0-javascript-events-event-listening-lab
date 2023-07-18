293447afb5092f69712d561753b686a0ba6bb1fa
tomutanyi <tomutanyi@gmail.com>
2023-07-18 13:36:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-function addingEventListener() {
+function addingEventListener() {const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
